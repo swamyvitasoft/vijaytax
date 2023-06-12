61bfc92fad57f17e5c11b697f26e4668e7b56ea3
stoswebsolutions <stoswebsolutions@gmail.com>
2023-06-12 17:27:31 +0530

    timezone baseurl index update

--- v1/app/Config/App.php
+++ v1/app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'https://vijaytax.vitasoft.in/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -42,7 +42,7 @@ class App extends BaseConfig
      * something else. If you are using mod_rewrite to remove the page set this
      * variable so that it is blank.
      */
-    public string $indexPage = 'index.php';
+    public string $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
@@ -110,7 +110,7 @@ class App extends BaseConfig
      *
      * @see https://www.php.net/manual/en/timezones.php for list of timezones supported by PHP.
      */
-    public string $appTimezone = 'UTC';
+    public string $appTimezone = 'Asia/Kolkata';
 
     /**
      * --------------------------------------------------------------------------
