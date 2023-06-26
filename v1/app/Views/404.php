5144d9d166427454c6a5ad3ed7a7f4894eb09ad5
stoswebsolutions <stoswebsolutions@gmail.com>
2023-06-26 15:46:52 +0530

    404 file not found

--- v1/app/Views/404.php
+++ v1/app/Views/404.php
@@ -1,4 +1,6 @@
+<?= view('common/top') ?>
 <?= view('common/header') ?>
-<h1 class="text-center">File Not Found</h1>
-<a href="<?= site_url() ?>dashboard/index" class="btn btn-success m-5">return Dashboard</a>
+<h1 class="text-center mt-5">File Not Found</h1>
+<a href="<?= site_url() ?>dashboard/index" class="btn btn-success mx-auto d-block">return Dashboard</a>
 <?= view('common/footer') ?>
+<?= view('common/bottom') ?>
